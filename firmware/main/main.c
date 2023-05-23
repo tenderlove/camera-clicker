e515be648407edc46607c1d0b39c4c01be9d4e81
Aaron Patterson <tenderlove@ruby-lang.org>
2023-05-23 16:48:18 -0700

    buttons are working

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -12,12 +12,18 @@
 #include "esp_flash.h"
 #include "wifi_manager.h"
 #include <esp_http_client.h>
+#include <driver/gpio.h>
 
 /* @brief tag used for ESP serial console messages */
 static const char TAG[] = "main";
 
 #define MAX_HTTP_RECV_BUFFER 512
 #define MAX_HTTP_OUTPUT_BUFFER 2048
+#define ESP_INTR_FLAG_DEFAULT 0
+
+#define GPIO_INPUT_IO_0     32
+#define GPIO_INPUT_IO_1     33
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
 
 static QueueHandle_t tx_task_queue;
 
@@ -25,6 +31,11 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
     return ESP_OK;
 }
+
+uint8_t mask0 = 0;
+uint8_t mask1 = 0;
+int io0 = 0;
+int io1 = 0;
 
 /**
  * @brief this is an exemple of a callback that you can setup in your own app to get notified of wifi manager event.
@@ -44,10 +55,39 @@ void cb_connection_ok(void *pvParameter){
 
 void monitoring_task(void *pvParameter)
 {
-	for(;;){
-		ESP_LOGI(TAG, "free heap: %"PRIu32, esp_get_free_heap_size());
-		vTaskDelay( pdMS_TO_TICKS(10000) );
-	}
+    for(;;){
+        ESP_LOGI(TAG, "free heap: %"PRIu32, esp_get_free_heap_size());
+        vTaskDelay( pdMS_TO_TICKS(10000) );
+    }
+}
+
+void button_task(void *pvParameter)
+{
+    for(;;){
+        uint8_t current0 = gpio_get_level(GPIO_INPUT_IO_0);
+        uint8_t current1 = gpio_get_level(GPIO_INPUT_IO_1);
+
+        mask0 = (mask0 << 1) | current0 | 0xF0;
+        mask1 = (mask1 << 1) | current1 | 0xF0;
+
+        // It's been high or low for 4 bits (4 * 5ms == 20ms)
+        if (mask0 == 0xF0 || mask0 == 0xFF) {
+            if (current0 != io0) {
+                io0 = current0;
+                unsigned int tx_action = GPIO_INPUT_IO_0;
+                xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
+            }
+        }
+
+        if (mask1 == 0xF0 || mask1 == 0xFF) {
+            if (current1 != io1) {
+                io1 = current1;
+                unsigned int tx_action = GPIO_INPUT_IO_1;
+                xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
+            }
+        }
+        vTaskDelay( pdMS_TO_TICKS(10) );
+    }
 }
 
 void
@@ -86,44 +126,56 @@ transmit_task(void *arg)
                 }
 
                 break;
+            case GPIO_INPUT_IO_0:
+                if (gpio_get_level(GPIO_INPUT_IO_0)) {
+                    printf("button 1 pressed\n");
+                } else {
+                    printf("button 1 released\n");
+                }
+                break;
+            case GPIO_INPUT_IO_1:
+                if (gpio_get_level(GPIO_INPUT_IO_1)) {
+                    printf("button 2 pressed\n");
+                } else {
+                    printf("button 2 released\n");
+                }
+                break;
         }
     }
 }
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    tx_task_queue = xQueueCreate(5, sizeof(uint32_t));
+    xTaskCreatePinnedToCore(transmit_task, "request task", 4096, NULL, 8, NULL, tskNO_AFFINITY);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_down_en = 1;
+    gpio_config(&io_conf);
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
+    io0 = gpio_get_level(GPIO_INPUT_IO_0);
+    io1 = gpio_get_level(GPIO_INPUT_IO_1);
+    if (io0) {
+        mask0 = 0xFF;
+    }
+    else {
+        mask0 = 0xF0;
     }
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    tx_task_queue = xQueueCreate(1, sizeof(uint32_t));
-    xTaskCreatePinnedToCore(transmit_task, "request task", 4096, NULL, 8, NULL, tskNO_AFFINITY);
+    if (io1) {
+        mask1 = 0xFF;
+    }
+    else {
+        mask1 = 0xF0;
+    }
 
     /* start the wifi manager */
     wifi_manager_start();
 
     wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
-    xTaskCreatePinnedToCore(&monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(monitoring_task, "monitoring_task", 2048, NULL, 1, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(button_task, "button_task", 4096, NULL, 9, NULL, tskNO_AFFINITY);
 }
